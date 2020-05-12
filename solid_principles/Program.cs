9d30580e4bc417196c29e518fcd58b68b6ef763a
Femi Ayodeji <feminstincts@gmail.com>
2020-05-12 22:06:40 +0100

    created solid prinples sample

---
+++ solid_principles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace solid_principles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
