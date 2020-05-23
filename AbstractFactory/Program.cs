c2e3f17e403a274b624ccee077270162c07300e3
Femi Ayodeji <feminstincts@gmail.com>
2020-05-23 23:21:26 +0100

    Test the design

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -6,7 +6,18 @@ namespace AbstractFactory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ABSTRACT FACTORY DESIGN PATTERN\n");
+            PackageAndDeliveryFactory standardFactoryObj = new StandardFactory();            
+            Client standardProduct = new Client(standardFactoryObj);
+            Console.WriteLine(standardProduct.ClientPackaging.GetType().ToString());
+            Console.WriteLine(standardProduct.ClientDocument.GetType().ToString());
+
+            Console.WriteLine("-------");
+
+            PackageAndDeliveryFactory delicateFactoryObj = new DelicateFactory();
+            Client delicateProduct = new Client(delicateFactoryObj);
+            Console.WriteLine(delicateProduct.ClientPackaging.GetType().ToString());
+            Console.WriteLine(delicateProduct.ClientDocument.GetType().ToString());
         }
     }
 }
