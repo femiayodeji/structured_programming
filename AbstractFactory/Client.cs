b87e1e83344191bd5b9d696239e735e01f050da5
Femi Ayodeji <feminstincts@gmail.com>
2020-05-23 22:35:09 +0100

    added factory classes

--- AbstractFactory/Client.cs
+++ AbstractFactory/Client.cs
@@ -5,3 +5,28 @@ public class ShockProofPacking : Packing{}
 public abstract class DeliveryDocument{}
 public class Postal : DeliveryDocument{}
 public class Courier : DeliveryDocument{}
+
+//Factory classes
+public abstract class PackageAndDeliveryFactory
+{
+    public abstract Packing CreatePackaging();
+    public abstract DeliveryDocument CreateDeliveryDocument();
+}
+public class StandardFactory : PackageAndDeliveryFactory
+{
+    public override Packing CreatePackaging(){
+        return new StandardPacking();
+    }
+    public override DeliveryDocument CreateDeliveryDocument(){
+        return new Postal();
+    }
+}
+public class DelicateFactory : PackageAndDeliveryFactory
+{
+    public override Packing CreatePackaging(){
+        return new ShockProofPacking();
+    }
+    public override DeliveryDocument CreateDeliveryDocument(){
+        return new Courier();
+    }
+}
