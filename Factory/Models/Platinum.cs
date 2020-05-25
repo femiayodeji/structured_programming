79d50dde85a374fb5a9492b98cb2080890c308cb
Femi Ayodeji <feminstincts@gmail.com>
2020-05-25 19:32:27 +0100

    added platinum creditcard

---
+++ Factory/Models/Platinum.cs
@@ -0,0 +1,27 @@
+namespace Factory.Models
+{
+    public class Platinum : CreditCard
+    {
+        private readonly string _cardType;
+        private int _creditLimit;
+        private int _annualCharge;
+        public Platinum(int creditLimit, int annualCharge)
+        {
+            _cardType = "Platinum";
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override string CardType{
+            get => _cardType;
+        }
+
+        public override int CreditLimit{
+            get => _creditLimit; set => _creditLimit = value;
+        }
+
+        public override int AnnualCharge{
+            get => _annualCharge; set => _annualCharge = value;
+        }
+    }
+}
