1af7623d86ced22ad3de92d91683b18bda79a149
Femi Ayodeji <feminstincts@gmail.com>
2020-05-25 19:30:07 +0100

    added moneyback creditcard

---
+++ Factory/Models/MoneyBack.cs
@@ -0,0 +1,27 @@
+namespace Factory.Models
+{
+    public class MoneyBack : CreditCard
+    {
+        private readonly string _creditType;   
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public MoneyBack(int creditLimit, int annualCharge)
+        {
+            _creditType = "MoneyBack";
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override string CardType {
+            get => _creditType;
+        }
+
+        public override int CreditLimit { 
+            get => _creditLimit; set => _creditLimit = value;
+        }
+        public override int AnnualCharge { 
+            get => _annualCharge; set => _annualCharge = value;
+        }
+    }
+}
