be7ead14ec540ff36dd56c70b767b3f9f1accce6
Femi Ayodeji <feminstincts@gmail.com>
2020-05-25 22:05:07 +0100

    added concrete creator class titanium factory

---
+++ Factory/Models/TitaniumFactory.cs
@@ -0,0 +1,18 @@
+namespace Factory.Models
+{
+    public class TitaniumFactory : CardFactory
+    {
+        private int _creditLimit;
+        private int _annualCharge;
+        
+        public TitaniumFactory(int creditLimit, int annualCharge)
+        {
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override CreditCard GetCreditCard(){
+            return new TitaniumCreditCard(_creditLimit, _annualCharge);
+        }
+    }
+}
