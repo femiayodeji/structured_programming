ccaf8737c4f5921838b961c7566a78b961ff4f7f
Femi Ayodeji <feminstincts@gmail.com>
2020-05-25 22:19:16 +0100

    instantiated demo for the products

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Factory.Models;
 
 namespace Factory
 {
@@ -6,7 +7,29 @@ namespace Factory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("FACTORY DESIGN PATERN\n");
+
+            CardFactory factory = null;
+            CreditCard creditCard = null;
+            
+            //MoneyBack
+            factory = new MoneyBackFactory(5000,0);
+            creditCard = factory.GetCreditCard();
+            Console.WriteLine("Card Type: {0} \nCredit Limit: {1} \nAnnual Charge: {2}", creditCard.CardType, creditCard.CreditLimit,creditCard.AnnualCharge);
+
+            Console.WriteLine("--------------");
+            
+            //Titanium
+            factory = new TitaniumFactory(10000,500);
+            creditCard = factory.GetCreditCard();
+            Console.WriteLine("Card Type: {0} \nCredit Limit: {1} \nAnnual Charge: {2}", creditCard.CardType, creditCard.CreditLimit,creditCard.AnnualCharge);
+
+            Console.WriteLine("--------------");
+            
+            //Platinum
+            factory = new PlatinumFactory(500000,1000);
+            creditCard = factory.GetCreditCard();
+            Console.WriteLine("Card Type: {0} \nCredit Limit: {1} \nAnnual Charge: {2}", creditCard.CardType, creditCard.CreditLimit,creditCard.AnnualCharge);
         }
     }
 }
