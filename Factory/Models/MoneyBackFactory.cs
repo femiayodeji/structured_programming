65115aeb91f9875e18b5ae6bdee8062b7af73327
Femi Ayodeji <feminstincts@gmail.com>
2020-05-25 22:00:38 +0100

    added concrete creator class moneyback factory

---
+++ Factory/Models/MoneyBackFactory.cs
@@ -0,0 +1,19 @@
+namespace Factory.Models
+{
+    // concrete creator
+    public class MoneyBackFactory : CardFactory
+    {
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public MoneyBackFactory(int creditLimit, int annualCharge)
+        {
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override CreditCard GetCreditCard(){
+            return new MoneyBackCreditCard(_creditLimit, _annualCharge);
+        }
+    }
+}
