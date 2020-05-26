4c6bef15afb52bde4df0f719eae3ee542d496564
Femi Ayodeji <feminstincts@gmail.com>
2020-05-26 19:44:31 +0100

    instantiated bridge design pattern

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -6,7 +6,15 @@ namespace Bridge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("BRIDGE DESIGN PATTERN\n");
+
+            NodeBridge nodeBridge = new NodeBridge();
+
+            NodeA nodeA = new NodeA();
+            NodeB nodeB = new NodeB();
+
+            Console.WriteLine(nodeBridge.Reach(nodeA));
+            Console.WriteLine(nodeBridge.Reach(nodeB));
         }
     }
 }
