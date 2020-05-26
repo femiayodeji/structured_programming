707403ff5260dd66e643042327c50565108b3276
Femi Ayodeji <feminstincts@gmail.com>
2020-05-26 17:06:42 +0100

    instantiated prototype design pattern

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -6,7 +6,33 @@ namespace Prototype
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("PROTOTYPE DESIGN PATTERN\n");
+
+            InvitationCard invitationCard = new InvitationCard();
+            //first object
+            invitationCard.Sender = "Femi";
+            invitationCard.Title = "Collaboration Invitation";
+            invitationCard.Content = "Hi guys! You are welcome to join me in learning software design pattern.";
+            invitationCard.To = "Femi";
+            invitationCard.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+
+            InvitationCard[] guests = new InvitationCard[3];
+            string[] guestsName = {"Dami","Kwaku","Atoke"};
+            int i = 0;
+            foreach(string name in guestsName){
+                guests[i] = invitationCard.Clone(invitationCard);
+                guests[i].To = guestsName[i];
+                i++;
+            }
+
+            // Print all invitation card 
+            foreach(InvitationCard guest in guests){
+                Console.WriteLine("To: {0}",guest.To);
+                Console.WriteLine("Title: {0}",guest.Title);
+                Console.WriteLine("Content: {0}",guest.Content);
+                Console.WriteLine("Date: {0}",guest.Date);
+                Console.WriteLine("--------");               
+            }
         }
     }
 }
