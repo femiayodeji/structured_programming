3a888eddbd8c56e5e3f52b2d7d9fcffebe489044
Femi Ayodeji <feminstincts@gmail.com>
2020-05-27 20:46:06 +0100

    added a feature class

--- Decorator/Car.cs
+++ Decorator/Car.cs
@@ -5,7 +5,13 @@ namespace Decorator
         public virtual string CarType() => "Simple car";
     }
 
-    public class ACCar : Car{
+    public class ACCar : Car 
+    {
         public override string CarType() => "AC Car";
+    }
+
+    public class SoundSystemCar : Car
+    {
+        public override string CarType() => "Sound system car";
     }
 }
