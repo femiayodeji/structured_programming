b1656514460c61347844693a6f59fc6f4bc6f8cd
Femi Ayodeji <feminstincts@gmail.com>
2020-05-28 11:06:32 +0100

    added name property to employee

--- Composite/Employee.cs
+++ Composite/Employee.cs
@@ -4,33 +4,38 @@ namespace Composite
 {
     public interface IEmployee
     {
+        string Name { get; set; }
         void Designation();
     }    
     public class CEO : IEmployee
     {
+        public virtual string Name { get; set; }
         public virtual void Designation(){
-            Console.WriteLine("The Chief Executive of the company.");
+            Console.WriteLine("{0} - The Chief Executive of the company.", Name);
         }
     }
 
     public class HeadManager1 : CEO, IEmployee
     {
+        public override string Name { get; set; }
         public override void Designation(){
-            Console.WriteLine("The Head Manager 1 of the company.");
+            Console.WriteLine("{0} - The Head Manager 1 of the company.", Name);
         }
     }
 
     public class HeadManager2 : CEO, IEmployee
     {
+        public override string Name { get; set; }
         public override void Designation(){
-            Console.WriteLine("The head Manager 2 of the company.");
+            Console.WriteLine("{0} - The head Manager 2 of the company.", Name);
         }
     }
 
     public class AreaManager : HeadManager1, IEmployee
     {
+        public override string Name { get; set; }
         public override void Designation(){
-            Console.WriteLine("The Area Manager under the HM1");
+            Console.WriteLine("{0} - The Area Manager under the HM1", Name);
         }
     }
 }
--- Composite/Program.cs
+++ Composite/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Composite
 {
@@ -6,7 +7,20 @@ namespace Composite
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("COMPOSITE DESIGN PATTERN\n");
+
+            IEmployee areaManager = new AreaManager();
+            IEmployee headManager1 = new HeadManager1();
+            IEmployee ceo = new CEO();
+
+            List<IEmployee> employees = new List<IEmployee>();
+            employees.Add(areaManager);
+            employees.Add(headManager1);
+            employees.Add(ceo);
+
+            foreach(IEmployee employee in employees){
+                employee.Designation();
+            }
         }
     }
 }
