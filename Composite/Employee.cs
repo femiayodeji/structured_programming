6339af2231da77b0908d2c5d572a8252521eeac1
Femi Ayodeji <feminstincts@gmail.com>
2020-05-28 10:44:00 +0100

    added the root class

--- Composite/Employee.cs
+++ Composite/Employee.cs
@@ -5,5 +5,12 @@ namespace Composite
     public interface IEmployee
     {
         void Designation();
+    }    
+    public class CEO : IEmployee
+    {
+        public virtual void Designation(){
+            Console.WriteLine("This is the Chief Executive of the company.");
+        }
     }
+
 }
