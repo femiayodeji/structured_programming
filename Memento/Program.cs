b76073538b295246ddf5e39d1265f564d3088e5c
Femi Ayodeji <feminstincts@gmail.com>
2020-06-06 14:51:58 +0100

    created test object

--- Memento/Program.cs
+++ Memento/Program.cs
@@ -6,7 +6,19 @@ namespace Memento
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("MEMENTO DESIGN PATTERN \n");
+
+            Console.WriteLine("--------");
+
+            Person person = new Person("Atoke","Femi");
+            Console.WriteLine("Initial full name is {0} {1}",person.FirstName,person.LastName);
+
+            person.Update("Adetayo","Oyegbile");
+            Console.WriteLine("Updated full name is {0} {1}",person.FirstName,person.LastName);
+
+            person.Revert();
+            Console.WriteLine("Reverted full name is {0} {1}",person.FirstName,person.LastName);
+
         }
     }
 }
