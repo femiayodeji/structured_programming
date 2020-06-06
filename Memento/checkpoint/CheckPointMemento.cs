0a488740ef652687debbc5b17003411530a51cf5
Femi Ayodeji <feminstincts@gmail.com>
2020-06-06 15:43:56 +0100

    added checkpoint memento

---
+++ Memento/checkpoint/CheckPointMemento.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Memento.CheckPoint{
+    public class CheckPointMemento
+    {
+        public int CheckPointLevel { get; set; }
+        public int CheckPointScore { get; set; }
+        public DateTime CheckPointTime { get; set; }
+
+        public CheckPointMemento(int checkPointLevel, int checkPointScore, DateTime checkPointTime)
+        {
+            CheckPointLevel = checkPointLevel;
+            CheckPointScore = checkPointScore;
+            CheckPointTime = checkPointTime;
+        }
+    }    
+}
