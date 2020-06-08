d2d66e6c305f798c661272a1fff1d0e7ce3b7263
Femi Ayodeji <feminstincts@gmail.com>
2020-06-08 13:16:30 +0100

    added restore object method

--- Memento/checkpoint/PlayerStatistics.cs
+++ Memento/checkpoint/PlayerStatistics.cs
@@ -14,6 +14,19 @@ namespace Memento.checkpoint
         {
             return new CheckPointMemento(playerStatistics.LevelId, playerStatistics.Score, playerStatistics.CheckPointTime);
         }        
+
+        public void RestoreCheckPoint(CheckPointMemento checkPointMemento)
+        {
+            LevelId = checkPointMemento.CheckPointLevel;
+            Score = checkPointMemento.CheckPointScore;
+            CheckPointTime = checkPointMemento.CheckPointTime;
+        }
+
+        public void PrintStatistics()
+        {
+            Console.WriteLine("Checkpoint time : {0}", CheckPointTime);
+            Console.WriteLine("Level : {0}", LevelId);
+            Console.WriteLine("Score : {0}", Score);
+        }
     }
 }
-        }
