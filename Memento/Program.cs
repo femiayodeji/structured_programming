c2b347057fc4f795c84a7a925b459900937ac542
Femi Ayodeji <feminstincts@gmail.com>
2020-06-08 16:07:21 +0100

    added step description

--- Memento/Program.cs
+++ Memento/Program.cs
@@ -25,29 +25,28 @@ namespace Memento
 
             Console.WriteLine("Game Player statistics");
 
-            // step 1
+            // step 1 - set initial data
             PlayerStatistics _statistics = new PlayerStatistics();
             _statistics.LevelId = 1;
             _statistics.Score = 100;
             _statistics.CheckPointTime = DateTime.Now;
             _statistics.PrintStatistics();
 
-            // step 2
+            // step 2 - create and store data in memento instance
             CheckPointCareTaker _careTaker = new CheckPointCareTaker();
             _careTaker.checkPoint = _statistics.CreateCheckPoint(_statistics);
             Console.WriteLine(Environment.NewLine);
 
-            // step 3
+            // step 3 -  change the data
             _statistics.LevelId = 4;
             _statistics.Score = 320;
             _statistics.CheckPointTime = DateTime.Now.Add(new TimeSpan(0, 0, 40, 0));
             _statistics.PrintStatistics();
             Console.WriteLine(Environment.NewLine);
 
-            // step 4
+            // step 4 - restore the initial data
             _statistics.RestoreCheckPoint(_careTaker.checkPoint);
             _statistics.PrintStatistics();
-            
 
         }
     }
