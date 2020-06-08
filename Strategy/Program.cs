17b2aaf865aa6bb4bcbe346121236b481a6af05f
Femi Ayodeji <feminstincts@gmail.com>
2020-06-08 18:15:45 +0100

    created driver class

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Strategy.Models;
 
 namespace Strategy
 {
@@ -6,7 +7,27 @@ namespace Strategy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("STRATEGY DESIGN PATTERN\n");
+
+            Console.WriteLine("--------");
+
+            //moves
+            IJumpBehaviour shortJump = new ShortJump();
+            IJumpBehaviour longJump = new LongJump();
+            IKickBehaviour tornadoKick = new TornadoKick();
+            
+            //create fighter
+            Fighter sango = new Sango(tornadoKick, shortJump);
+            sango.Display();
+
+            //fight
+            sango.Punch();
+            sango.Kick();
+            sango.Jump();
+
+            //change behaviour dynamically(runtime)
+            sango.setKickBehaviour(longJump);
+            sango.Jump();
         }
     }
 }
