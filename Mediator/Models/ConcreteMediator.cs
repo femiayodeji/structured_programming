9dca550b1f958389711d5726b63e4bda16344f82
Femi Ayodeji <feminstincts@gmail.com>
2020-06-17 19:58:25 +0100

    added mediator send message logic

--- Mediator/Models/ConcreteMediator.cs
+++ Mediator/Models/ConcreteMediator.cs
@@ -2,9 +2,16 @@ namespace Mediator.Models
 {
     public class ConcreteMediator : IMediator
     {
+        public UserA userA { get; set; }
+        public UserB userB { get; set; }
         public void SendMessage(User caller, string msg)
         {
-            throw new System.NotImplementedException();
+            if(caller == userA){
+                userB.Receive(msg);
+            }
+            else{
+                userA.Receive(msg);
+            }
         }
     }
 }
