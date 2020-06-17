483692d0ec4b9975ff6c3fb0aec650b5affb030b
Femi Ayodeji <feminstincts@gmail.com>
2020-06-17 18:14:19 +0100

    added mediator concrete class

---
+++ Mediator/Models/ConcreteMediator.cs
@@ -0,0 +1,10 @@
+namespace Mediator.Models
+{
+    public class ConcreteMediator : IMediator
+    {
+        public void SendMessage(User caller, string msg)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
