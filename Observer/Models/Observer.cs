0f1831e96ccdee96babe7ba07b2177cdee63308c
Femi Ayodeji <feminstincts@gmail.com>
2020-06-21 12:49:20 +0100

    implemented the interface

--- Observer/Models/Observer.cs
+++ Observer/Models/Observer.cs
@@ -1,7 +1,16 @@
+using System;
+
 namespace Obverser.Models
 {
-    interface IObserver
+    public interface IObserver
     {
-        void Changed(Object obj)
+        void Changed(Object obj);
+    }
+
+    public class HeaderColor : IObserver
+    {
+        public void Changed(Object color){
+            Console.WriteLine("Header Color is {0} ", color.ToString());
+        }
     }
 }
--- Observer/Models/Subject.cs
+++ Observer/Models/Subject.cs
@@ -1,9 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Obverser.Models
 {
-    interface ISubject
+    public interface ISubject
     {
         void Register(IObserver Observer);
         void Unregister(IObserver Observer);
         void Notify();
+    }
+
+    public class ColorSubject : ISubject
+    {
+        private string _color = "blue";
+        public string Color {
+            get => _color;
+            set{
+                _color = value;
+                Notify();
+            }
+        }       
+
+        private HashSet<IObserver> _observers = new HashSet<IObserver>();
+
+        public void Register(IObserver observer) => _observers.Add(observer);
+
+        public void Unregister(IObserver observer) => _observers.Remove(observer);
+        public void Notify() => _observers.ToList().ForEach(observer => observer.Changed(Color));
     }
 }
